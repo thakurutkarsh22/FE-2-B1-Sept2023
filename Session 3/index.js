c309976fcece082d2447cd989cde7704db4e00e1
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-09-08 22:03:30 +0530

    session 3

---
+++ Session 3/index.js
@@ -0,0 +1,11 @@
+//  ------------> Deployment -------.
+
+// HOISTING -> javascript concept
+
+// VS
+
+// HOSTING -> Sending the Files to the server so that other users can Get those files and they can see otr web page.
+
+// FREE HOSTING -> Netlify...
+
+// LEARN github action ... THIS IS FOR CONTINUE DEPLOYMENT (without intervention)
